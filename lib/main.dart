707009396a68415f38cde59d0f270054a73a3936
mstfb <hmustafabolat@gmail.com>
2025-07-08 00:58:37 +0300

    firebase add

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mbtest/screens/login_screen.dart';
 import 'screens/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Sipariş Takip',
-    theme: ThemeData(primarySwatch: Colors.indigo),
-    debugShowCheckedModeBanner: false,
-    home: const LoginScreen(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    MaterialApp(
+      title: 'Sipariş Takip',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    ),
+  );
 }
